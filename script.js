010d4fccc155e12bcfd9f22dfbfa6d14d57a3078
Natzabes <sr786382@gmail.com>
2025-05-26 10:16:42 -0400

    Agregado de fecha y modo edicion

--- script.js
+++ script.js
@@ -1,65 +1,149 @@
-const students=[];
+const students = [];
 
-const tableBody=document.querySelector("#studentsTable tbody");
-const averageDiv=document.getElementById("average");
+const spanAverage = document.getElementById("average-grade");
+const tableBody = document.querySelector("#studentsTable tbody")
+const form = document.getElementById("studentForm");
+const editBtn = document.getElementById("form-edit");
+let estaEnModoEditar = false;
 
-document.getElementById("studentForm").addEventListener("submit",function(e){
-    e.preventDefault();
-
-    const name=document.getElementById("name").value.trim();
-    const lastName=document.getElementById("lastName").value.trim();
-    const grade=parseFloat(document.getElementById("grade").value.trim());
-
-    if(grade <1 || grade >7 || !name || !lastName || isNaN(grade)){
-    alert("Error Datos Incorrectos")
-    return
+function calcularPromedio() {
+    if (students.length === 0) return spanAverage.textContent = `No Disponible`;
+    if (students.length === 1) return spanAverage.textContent = `${students[0].grade}`;
+    let average = 0
+    
+    for (let i = 0; i < students.length; i++) {
+        average += Math.floor(students[i].grade * 100) * 0.01;
     }
-
-    //guardar datos en el Array nuevo
-
-    const student={name,lastName,grade};
-    students.push(student);
-     addStudentToTable(student)
-     calcularPromedio()
-   // console.log(students)
-
-    this.reset();
-
-});
-
-function addStudentToTable(student){
-    const row=document.createElement("tr");
-    row.innerHTML=
-    `<td>${student.name}</td>
-     <td>${student.lastName}</td>
-    <td>${student.grade}</td>
-    <td></td>
-    <td><button class="delete">Eliminar</button> <button class="edit">Editar</button></td>
-    `;
- row.querySelector(".delete").addEventListener("click",function(){
-    deleteEstudiante(student,row);
- });
-   tableBody.appendChild(row);
+    average = average / students.length;
+    return average.toFixed(1);
 }
 
-function deleteEstudiante(student,row){
-    //Buscar el estudiante por el array
-    const index=students.indexOf(student);
-    if(index > -1){
-        students.splice(index,1);
-        row.remove();
-        calcularPromedio();
-    }
- };
+function actualizarDisplayPromedio() {
+    spanAverage.textContent = calcularPromedio();
+}
 
-function calcularPromedio(){
-    if(students.length===0){
-       averageDiv.textContent="Promedio General del Curso:N/A"
-        return;
+function encontrarFilaSelec() {
+    const listaFilas = document.querySelectorAll("tbody tr");
+    let filaEncontrada = null;
+    listaFilas.forEach((fila) => {
+        if(fila.querySelector(".edit").checked) filaEncontrada = fila;
+    })
+    return filaEncontrada;
+}
+
+function formSubmit(e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const lastName = document.getElementById("lastName").value.trim();
+    const grade = document.getElementById("grade").value.trim();
+    const date = document.getElementById("date").value.trim();
+    form.reset();
+
+    const student = {name, lastName, grade, date};
+    students.push(student);
+    actualizarDisplayPromedio();
+    addStudentToTable(student);
+}
+form.onsubmit = formSubmit;
+
+function cambiarEstiloForm() {
+    const formBtn = document.getElementById("form-btn");
+    const divForm = document.getElementById("form-container");
+    const radioList = document.querySelectorAll("input.edit");
+    form.reset()
+    if(!estaEnModoEditar) {
+        divForm.classList.remove("border-warning");
+        divForm.classList.add("border-primary");
+
+        formBtn.textContent = "Guardar Estudiante"
+        formBtn.classList.remove("btn-warning");
+        formBtn.classList.add("btn-primary");
+        form.onsubmit = formSubmit;
+
+        radioList.forEach((radio) => radio.setAttribute("disabled", ""))
+        editBtn.textContent = "Modo Edición";
+        let fila = encontrarFilaSelec()
+        if(fila) encontrarFilaSelec().querySelector(".edit").checked = false;
+        return
     }
-    const total=students.reduce((sum,s)=>sum+s.grade,0);
-    console.log(total)
-    const average=total/students.length;
-    console.log(average)
-    averageDiv.textContent=`Promedio General del Curso: ${average.toFixed(2)}`;
+    divForm.classList.remove("border-primary");
+    divForm.classList.add("border-warning");
+    
+    formBtn.textContent = "Editar Estudiante"
+    formBtn.classList.remove("btn-primary");
+    formBtn.classList.add("btn-warning");
+
+    radioList.forEach((radio) => radio.removeAttribute("disabled"))
+    editBtn.textContent = "Salir Edición";
+}
+
+editBtn.addEventListener("click", (e) => {
+    estaEnModoEditar = !estaEnModoEditar;
+    cambiarEstiloForm()
+})
+
+function eliminarEstudiante(student, row) {
+    const index = students.indexOf(student, row);
+    if(index > -1) {
+        students.splice(index, 1);
+        row.remove();
+        actualizarDisplayPromedio();
+    }
+}
+
+function addStudentToTable(student) {
+    const row = document.createElement("tr");
+    row.innerHTML = 
+    `
+        <td class="colDato">${student.name}</td>
+        <td class="colDato">${student.lastName}</td>
+        <td class="colDato">${student.grade}</td>
+        <td class="colDato">${student.date}</td>
+        <td class="colAcciones">
+            <button class="btn btn-danger mb-1 delete">Eliminar</button>
+            <div class="container column text-center div_edit">
+                <p class="mb-0">Editar:</p>
+                <input type="radio" class="form-check-input edit" name="edit" disabled>
+            </div>
+        </td>
+    `;
+    const btnDelete = row.getElementsByClassName("delete")[0];
+    const radioEdit = row.getElementsByClassName("edit")[0];
+
+    btnDelete.addEventListener("click", function(e) {
+        if(radioEdit.checked) {
+            estaEnModoEditar = false;
+            cambiarEstiloForm()
+        };
+        eliminarEstudiante(student, row);
+    });
+
+    radioEdit.addEventListener("click", function(e) {
+        const inputs = document.querySelectorAll("input.form-control");
+        inputs[0].value = student.name
+        inputs[1].value = student.lastName
+        inputs[2].value = student.grade
+        inputs[3].value = student.date
+        
+        form.onsubmit = function(e) {
+            e.preventDefault();
+            
+            student.name = inputs[0].value.trim();
+            student.lastName = inputs[1].value.trim();
+            student.grade = inputs[2].value.trim();
+            student.date = inputs[3].value.trim();
+
+            const cols = encontrarFilaSelec().querySelectorAll(".colDato");
+            cols[0].textContent = `${student.name}`;
+            cols[1].textContent = `${student.lastName}`;
+            cols[2].textContent = `${student.grade}`;
+            cols[3].textContent = `${student.date}`;
+
+            actualizarDisplayPromedio();
+            estaEnModoEditar = false;
+            cambiarEstiloForm();
+        }
+    });
+    tableBody.appendChild(row);
 }
